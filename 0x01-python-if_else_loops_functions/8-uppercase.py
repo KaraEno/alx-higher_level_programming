0dd5fe8ff4324fbbd81969c3b95ba5f65220f9c8
Abasiakara <abasiakaraalexander@gmail.com>
2022-09-20 17:34:15 -0700

    Tasks 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    return (ord(str) in range(ord('A'), ord('Z') + 1)
+    print("".join(["{:c}".format(ord(c)-32)
+          if ord(c) in range(97, 123) else c
+          for c in str]))
