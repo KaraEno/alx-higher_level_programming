d86cdd32fb03b588dc50d906410af6bcaab85fff
Abasiakara <abasiakaraalexander@gmail.com>
2022-09-28 13:45:24 -0700

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-    
+
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda y: [x**2 for x in y]), matrix)
+    """
+    compute the square of all integers of a matrix
+    args: nested list
+    Return: square of eacc integer in the array
+    """
+
+    return list(map(lambda y: [x**2 for x in y], matrix))
+
+if __name__ == '__main__':
+    matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
