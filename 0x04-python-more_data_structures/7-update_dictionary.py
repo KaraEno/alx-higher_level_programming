0ec1907df513926f10e1b861415e31fdb38df9c1
Abasiakara <abasiakaraalexander@gmail.com>
2022-09-29 13:13:28 -0700

    Task

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,7 +4,7 @@ def update_dictionary(a_dictionary, key, value):
     """
     add or replace a new key value in dict
     """
-    if a_dictionary[key] is None:
+    if a_dictionary is None:
         return None
     a_dictionary[key] = value
     return a_dictionary
