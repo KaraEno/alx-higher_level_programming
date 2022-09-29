7baf213f81448b1effdbca4d26ee1f337aaa7bcc
Abasiakara <abasiakaraalexander@gmail.com>
2022-09-29 10:06:55 -0700

    Task

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary):
-        b = "{} : {}".format(i, a_dictionary[i])
-    return b
+    """
+    print a dict in sorted order of keys
+    """
+    if a_dictionary is None:
+        return None
+    for key in sorted(a_dictionary.keys()):
+        print("{} : {}".format(key, a_dictionary[key]))
 if __name__ == '__main__':
     a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
     print_sorted_dictionary(a_dictionary)
