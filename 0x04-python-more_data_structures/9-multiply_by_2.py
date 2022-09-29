0d29635f63ff89a03ee6755e902e42c9b26c9328
Abasiakara <abasiakaraalexander@gmail.com>
2022-09-29 14:32:55 -0700

    Task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
-
-def multiply_by_2(a_dictionary):
-    for key, value in a_dictionary.items():
-        print("{:s} : {:d}".format(key, value * 2))
-    return list(a_dictionary)
+def multiply_by_2(a_dict):
+    """
+    add or replace a new key value in dict
+    """
+    if a_dict is None:
+        return None
+    return {
+        key: a_dict[key] * 2 for key in a_dict
+    }
